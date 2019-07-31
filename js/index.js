b75a04b6c912bf3b3358b85e484092677c9e8826
Aluno <renanandreolla@outlook.com>
2019-07-31 20:05:35 -0300

    Alterações no código, mas não finalizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 //Números Múltiplos
 var a = parseInt(prompt("Digite um número:"));
 var b = parseInt(prompt("Digite outro número:"));
+
+if(a > b) {
+  if(a % b === 0)
+document.body.append("São múltiplos")
+}else {
+  if(a < b) {
+    if(a % b !==0)
+  document.body.append("Não são múltiplos")
+}}
+// % = resto da divisão
