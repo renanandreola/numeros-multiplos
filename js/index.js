cc0f98222f05a1a11491645d75ff180ff57b4c37
Aluno <renanandreolla@outlook.com>
2019-07-31 20:27:20 -0300

    Correção do código, funcionando

--- js/index.js
+++ js/index.js
@@ -1,13 +1,21 @@
 //Números Múltiplos
+//4) Elaborar um programa em Javascript que lê 2 valores a e b e
+//os escreve com a mensagem: ?São múltiplos? ou ?Não são múltiplos?
 var a = parseInt(prompt("Digite um número:"));
 var b = parseInt(prompt("Digite outro número:"));
 
 if(a > b) {
-  if(a % b === 0)
-document.body.append("São múltiplos")
+  if(a % b === 0) {
+      document.body.append("São múltiplos")
+  } else {
+    document.body.append("Nao são múltiplos")
+  }
+
 }else {
-  if(a < b) {
-    if(a % b !==0)
-  document.body.append("Não são múltiplos")
-}}
+    if(b % a === 0) {
+        document.body.append("São múltiplos")
+    } else {
+      document.body.append("Nao são múltiplos")
+    }
+}
 // % = resto da divisão
